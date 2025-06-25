a41408091e35f97b7b68520c014b9865ca46a30c
samuel10iman <112088761+samuel10iman@users.noreply.github.com>
2025-06-25 11:00:40 -0500

    Update actualizar_perfil.php

--- actualizar_perfil.php
+++ actualizar_perfil.php
@@ -20,13 +20,34 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 
     if (!empty($nueva_contraseña)) {
-// PRO-02: actualizar_perfil_con_contraseña (IN correo_in VARCHAR(100), IN nombre_in VARCHAR(100), IN numero_in INT, 
+// PRO-03: actualizar_perfil_con_contraseña (IN correo_in VARCHAR(100), IN nombre_in VARCHAR(100), IN numero_in INT, 
 // IN contraseña_in VARCHAR(100)): Actualiza los datos de un usuario incluyendo su contraseña
+// 1. Se abre una llamada al procedimiento almacenado UpdateUsuario con 4 parámetros y se almacenará en la variavle $stmt.
+// 2. Tipos de parámtros:
+//    s = string, i = integer
+// 3. Se ejecuta el procedimiento almacenado pasando los valores proporcionados por el usuario mediante execute().
+// 4. Buscar en la tabla usuario el registro cuyo correo sea igual a correo_in.
+//    Una vez ejecutado este bloque, hay 2 posibles resultados:
+//    - Si existe, actualizará:
+//      nombre -> nombre_in
+//      numero -> numero_in
+//      contraseña -> contraseña_in
+//    - Si no existe, no realizará cambios.
         $stmt = $conn->prepare("CALL UpdateUsuario(?, ?, ?, ?)");
         $stmt->bind_param("siss", $nuevo_nombre, $nuevo_numero, $nueva_contraseña, $correo);
     } else {
-// PRO-03: actualizar_perfil_con_contraseña (IN correo_in VARCHAR(100), IN nombre_in VARCHAR(100), IN numero_in INT): 
+// PRO-04: actualizar_perfil_con_contraseña (IN correo_in VARCHAR(100), IN nombre_in VARCHAR(100), IN numero_in INT): 
 // Actualiza los datos de un usuario sin incluir su contraseña
+// 1. Se prepara el llamado al procedimiento almacenado UpdateUsuarioNombreNumero con 3 parámetros y se almacena en la variable $stmt.
+// 2. Tipos de parámetros:
+//    - s = string, i = integer
+// 3. El procedimiento almacenado realiza la actualización de los campos nombre y numero en la tabla usuario, buscando el registro por el correo recibido.
+// 4. Buscar en la tabla usuario el registro cuyo correo sea igual a correo_in.
+//    Una vez ejecutado este bloque, hay 2 posibles resultados:
+//    - Si existe, actualizará:
+//      nombre -> nombre_in
+//      numero -> numero_in
+//    - Si no existe, no realizará cambios.
         $stmt = $conn->prepare("CALL UpdateUsuarioNombreNumero(?, ?, ?)");
         $stmt->bind_param("sis", $nuevo_nombre, $nuevo_numero, $correo);
     }
