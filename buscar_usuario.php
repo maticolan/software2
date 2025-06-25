b6f49add6608e169b1834ae0826277b9cd2fda39
samuel10iman <112088761+samuel10iman@users.noreply.github.com>
2025-06-25 11:39:42 -0500

    Update buscar_usuario.php

--- buscar_usuario.php
+++ buscar_usuario.php
@@ -6,7 +6,16 @@ $correo_emisor = $_SESSION['correo'] ?? '';
 $q = $_GET['q'] ?? '';
 
 if (strlen($q) >= 2) {
-    // PRO-14: 
+    // PRO-14: Permite buscar usuarios (clientes) por nombre o correo para mostrarlos como posibles destinatarios en operaciones de regalo o transferencia dentro de la plataforma. Además, excluye al propio usuario que está realizando la búsqueda (para que no pueda seleccionarse a sí mismo como destinatario).
+    // 1. Se prepara la ejecución del procedimiento GetUsuariosPorBusqueda(), que recibe dos parámetros:
+    //    - q -> texto de búsqeuda parcial
+    //    - correo_emisor -> el correo del usuario actual
+    // 2. Se asignan los valores de los parámetros al procedimiento:
+    //    - s -> string
+    //    - $q -> texto parcial que busca coincidencias
+    //    - $correo_emisor ->  el usuario actual, que será excluido de los resultados.
+    // 3. Se ejecuta el procedimiento almacenado con los valores de búsqueda proporcionados.
+    // 4. Se recupera el conjunto de resultados (usuarios encontrados que cumplen los filtros).
     $stmt = $conn->prepare("CALL GetUsuariosPorBusqueda(?, ?)");
     $stmt->bind_param("ss", $q, $correo_emisor);
     $stmt->execute();
